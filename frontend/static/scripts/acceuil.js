28b7cf3902c8761e03a9e83eeaca5e98cf274e7c
Hamabarhamou <hama.peinture.barhamou@gmail.com>
2022-11-10 12:04:14 +0100

    ajout d'image #9

--- frontend/static/scripts/acceuil.js
+++ frontend/static/scripts/acceuil.js
@@ -1,1 +1,35 @@
 console.log('hello acceuil')
+/**
+ * retrieve the list of item categories and display them
+ */
+
+categorie = [
+    {
+        "id":"1",
+        "src":"https://www.shutterstock.com/image-photo/document-management-system-dms-being-600w-1874749972.jpg",
+        "title":"Oridnateurs"
+    },
+    {
+        "id":"2",
+        "src":"https://www.shutterstock.com/image-photo/smartphone-portable-game-consoles-ebook-600w-1827624617.jpg",
+        "title":"Smart Phone"
+    },
+    {
+        "id":"3",
+        "src":"https://www.shutterstock.com/image-vector/semiconductor-electronic-circuit-board-isometric-600w-565557181.jpg",
+        "title":"Composant electronique"
+    },
+    {
+        "id":"4",
+        "src":"https://farm3.staticflickr.com/2403/2127291233_899c16de27_d.jpg",
+        "title":"Flickr picture by sea turtle"
+    }
+]
+
+$(document).ready(function () {
+    let code=''
+    categorie.forEach(element => {
+        code += '<img src="'+element.src+'"title="'+element.title+'">'
+    });
+    $('#store').html(code)
+})
