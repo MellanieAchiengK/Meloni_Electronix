5c0caed93ec539fcfd5c493e5dd477a0d859dbee
Hamabarhamou <hama.peinture.barhamou@gmail.com>
2022-11-15 21:43:29 +0100

    implementation api DELET POST and GET Contry #5

--- api/v1/views/Contry.py
+++ api/v1/views/Contry.py
@@ -3,9 +3,10 @@
 Liste of the produit
 """
 
-from flask import jsonify
+from flask import jsonify, request, abort
 from views import app_views
-
+from models.country import Country
+from models import storage
 
 
 @app_views.route(
@@ -13,5 +14,37 @@ from views import app_views
     methods=['GET'],
     strict_slashes=False)
 def contry_all():
-    return jsonify({'contry':"contry"})
+    countries = storage.all(Country).values()
+    liste = []
+    for loop in countries:
+        liste.append(loop.to_dict())
+    return liste, 200
+    
 
+@app_views.route(
+    '/contry',
+    methods=['POST'],
+    strict_slashes=False)
+def createContry():
+    contries = storage.all(Country)
+    data = request.get_json()
+
+    if type(data) is not dict:
+        abort(404, "Not a JSON")
+    if "name" not in data:
+        abort(400, "Missing name")
+    contry = Country(**data)
+    storage.new(contry)
+    storage.save()
+    return jsonify(contry.to_dict()), 201
+
+
+@app_views.route('/contry/<state_id>',
+                 strict_slashes=False, methods=['DELETE'])
+def delSate(state_id):
+    state = storage.get(Country, state_id)
+    if state is None:
+        abort(404)
+    storage.delete(state)
+    storage.save()
+    return jsonify({}), 200
