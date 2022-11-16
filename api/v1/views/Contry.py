6417ad86eb0f98f40cedf39eac3d8cc43635c7cd
Hamabarhamou <hama.peinture.barhamou@gmail.com>
2022-11-16 01:09:01 +0100

    add end point get id by name for country

--- api/v1/views/Contry.py
+++ api/v1/views/Contry.py
@@ -10,7 +10,7 @@ from models import storage
 
 
 @app_views.route(
-    '/contry',
+    '/country',
     methods=['GET'],
     strict_slashes=False)
 def contry_all():
@@ -22,7 +22,7 @@ def contry_all():
     
 
 @app_views.route(
-    '/contry',
+    '/country',
     methods=['POST'],
     strict_slashes=False)
 def createContry():
@@ -39,12 +39,45 @@ def createContry():
     return jsonify(contry.to_dict()), 201
 
 
-@app_views.route('/contry/<state_id>',
+@app_views.route('/country/<country_id>',
                  strict_slashes=False, methods=['DELETE'])
-def delSate(state_id):
-    state = storage.get(Country, state_id)
+def delCounty(country_id):
+    state = storage.get(Country, country_id)
     if state is None:
         abort(404)
     storage.delete(state)
     storage.save()
     return jsonify({}), 200
+
+@app_views.route('/country/<country_id>',
+                 strict_slashes=False, methods=['PUT'])
+def updateContry(country_id):
+    data = request.get_json()
+
+    if type(data) is not dict:
+        abort(404, "Not a JSON")
+    if "name" not in data:
+        abort(400, "Missing name")
+
+    state = storage.get(Country, country_id)
+    if state is None:
+        abort(404)
+    black_list = ["id", "created_at", "updated_at"]
+    for key, value in data.items():
+        if key not in black_list:
+            setattr(state, key, value)
+    storage.save()
+    return jsonify(state.to_dict()), 200
+
+
+@app_views.route('/country/<country_name>',
+                 strict_slashes=False, methods=['GET'])
+def get_id_country_name(country_name):
+    countries = storage.all(Country).values()
+    id = None
+    for loop in countries:
+        dic = loop.to_dict()
+        print(dic['name'])
+        if country_name == dic['name']:
+            id = dic['id']
+    return jsonify({"id": id}), 200
