7f24d7a215e67b067a83344647a9fefcdfdeb2c8
Hamabarhamou <hama.peinture.barhamou@gmail.com>
2022-11-17 10:02:54 +0100

    Implementation non testé des and point de user #5

---
+++ api/v1/views/User.py
@@ -0,0 +1,82 @@
+#!/usr/bin/python3
+"""
+Liste of the produit
+"""
+
+from flask import jsonify, request, abort
+from views import app_views
+from models.user import User
+from models import storage
+
+
+@app_views.route(
+    '/users',
+    methods=['GET'],
+    strict_slashes=False)
+def users_all():
+    users = storage.all(User).values()
+    liste = []
+    for loop in users:
+        liste.append(loop.to_dict())
+    return liste, 200
+    
+
+@app_views.route(
+    '/user',
+    methods=['POST'],
+    strict_slashes=False)
+def createUser():
+    data = request.get_json()
+
+    if type(data) is not dict:
+        abort(404, "Not a JSON")
+    if "name" not in data:
+        abort(400, "Missing name")
+    user = User(**data)
+    storage.new(user)
+    storage.save()
+    return jsonify(user.to_dict()), 201
+
+
+@app_views.route('/user/<user_id>',
+                 strict_slashes=False, methods=['DELETE'])
+def delUser(user_id):
+    user = storage.get(User, user_id)
+    if user is None:
+        abort(404)
+    storage.delete(user)
+    storage.save()
+    return jsonify({}), 200
+
+@app_views.route('/user/<user_id>',
+                 strict_slashes=False, methods=['PUT'])
+def updateUser(user_id):
+    data = request.get_json()
+
+    if type(data) is not dict:
+        abort(404, "Not a JSON")
+    if "name" not in data:
+        abort(400, "Missing name")
+
+    user = storage.get(User, user_id)
+    if user is None:
+        abort(404)
+    black_list = ["id", "created_at", "updated_at"]
+    for key, value in data.items():
+        if key not in black_list:
+            setattr(user, key, value)
+    storage.save()
+    return jsonify(user.to_dict()), 200
+
+
+""" @app_views.route('/user/<user_id>',
+                 strict_slashes=False, methods=['GET'])
+def get_id_user_name(citie_name):
+    users = storage.all(User).values()
+    id = None
+    for loop in users:
+        dic = loop.to_dict()
+        print(dic['name'])
+        if citie_name == dic['name']:
+            id = dic['id']
+    return jsonify({"id": str(id)}), 200  """
