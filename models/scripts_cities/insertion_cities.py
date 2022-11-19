3812e4d3bd9393f0dcb328ebacb46df2ae666a85
Hamabarhamou <hama.peinture.barhamou@gmail.com>
2022-11-19 13:31:59 +0100

    Finalisation du scrpt d'insertion des valeur des ville dans la bases de données #16

--- models/scripts_cities/insertion_cities.py
+++ models/scripts_cities/insertion_cities.py
@@ -4,22 +4,24 @@ from listeFile import liste_Files
 from models import storage
 from models.city import City
 import os
+from os import listdir
+from os.path import isfile, join
 
 os.getcwd()
+PATH_COUNTIRES_FILE = os.getcwd() + '/models/scripts_cities/countries'
 PATH_FILE_BY_CONTRIES = os.getcwd() + '/models/scripts_cities/cities'
+files_countries = [f for f in listdir(PATH_FILE_BY_CONTRIES) if isfile(join(PATH_FILE_BY_CONTRIES, f))]
 
 
 id = 1
-for country in liste_Files():
-    #print(id," ",country)
-    with open(PATH_FILE_BY_CONTRIES + "/" + country, "r") as filin:
-        ligne = filin.readline()
-        while ligne != "":
-            print(ligne)
-            if ligne is not None:
-                city = City(name=ligne, country_id=id)
-                print(city.to_dict())
 
-            ligne = filin.readline()
+for country in sorted(files_countries):
+    #print(id,"------------------------------ ",country)
+    cities_all = liste_Files(PATH_FILE_BY_CONTRIES + "/" + country)
+    if cities_all is not None:
+        for cit in cities_all:
+            city = City(name=cit, country_id=id)
+            city.save()
+            print(city.to_dict())
     id += 1
 
