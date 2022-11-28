5e583d87594f064d7f23efcd31dbd7e55aab598f
Mellanie <mellanie.achieng@gmail.com>
2022-11-28 19:06:56 +0000

    homepage updated #7

--- web/frontend/static/scripts/footer.js
+++ web/frontend/static/scripts/footer.js
@@ -2,14 +2,13 @@ const createFooter = () => {
     let footer = document.querySelector('footer');
 
     footer.innerHTML = `
-    <div class="footer-contact-container">
+    <div class="footer-social-container">
         <div>
-           <p><image src="{{ url_for('static', filename='fronted/static/images/phone-call.png') }}">0712345678</p>
-           <p><image src="{{ url_for('static', filename='frontend/static/images/email.png') }}">melonielectronix@gmail.com</p>
-           <p><image src="{{ url_for('static', filename='frontend/static/images/whatsapp.png') }}">0712345678</p>
-           <p><image src="{{ url_for('static', filename='frontend/static/images/instagram.png') }}">@melonielectronix</p> 
+           <a href="#" class="fa fa-phone"></a>
+           <a href="#" class="fa fa-envelope"></a>
+           <a href="#" class="fa fa-whatsapp"></a>
+           <a href="#" class="fa fa-instagram"></a>
     </div>
-    <p class="footer-credit">Quality and Affordable</p>
     `;
 }
 
